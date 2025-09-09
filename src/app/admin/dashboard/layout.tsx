5a2104ea2e55e50944bc6e0153961959a60fb97c
Hyzyr <avezberdiyew@gmail.com>
2025-09-09 23:35:21 +0500

    update admin layout

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -1,36 +1,16 @@
 import '@/UI/assets/css/main.scss';
 import HeaderDashboard from '@/UI/base/Header/HeaderDashboard';
-
-import type { Metadata } from 'next';
-import { Manrope } from 'next/font/google';
 import AdminGuard from './AdminGuard';
-
-const manrope = Manrope({
-  subsets: ['latin'],
-  weight: ['200', '300', '400', '500', '600', '700'],
-  display: 'swap',
-  variable: '--font-manrope',
-});
 
 type RootPropsType = {
   children: React.ReactNode;
 };
-export const metadata: Metadata = {
-  title: 'Nurly Tajir | Dowrebap Tehnologiya',
-  description: 'Nurly Tajir | Dowrebap Tehnologiya',
-};
 
-export default async function RootLayout({ children }: RootPropsType) {
+export default async function Layout({ children }: RootPropsType) {
   return (
-    <html lang={'en'}>
-      <body className={`${manrope.variable} `}>
-        <AdminGuard>
-          <main className={'main'} id="main">
-            <HeaderDashboard />
-            {children}
-          </main>
-        </AdminGuard>
-      </body>
-    </html>
+    <AdminGuard>
+      <HeaderDashboard />
+      {children}
+    </AdminGuard>
   );
 }
